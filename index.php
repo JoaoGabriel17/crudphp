bed6708ac57ab20dd59ef9bad8ebbc0b77215175
João Gabriel <joaogabrielgojuryu@gmail.com>
2023-05-16 15:56:46 -0300

    add formulario

--- index.php
+++ index.php
@@ -1,12 +1,56 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CRUD</title>
-</head>
-<body>
-    <h1>Testando commit</h1>
-</body>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Cadastro</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+  </head>
+  <body>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>Cadastro</h1>
+
+                <form action = "cadastro_script.php" methode = "POST">
+                  <div class="form-group">
+                    <label for="nome">Nome Completo:</label>
+                    <input type="text" class="form-control" name="nome">
+                  </div>
+
+                  <div class="form-group">
+                    <label for="endereco">Endereço:</label>
+                    <input type="text" class="form-control" name="endereco">
+                  </div>
+
+                  <div class="form-group">
+                    <label for="telefone">Telefone:</label>
+                    <input type="text" class="form-control" name="telefone">
+                  </div>
+
+                  <div class="form-group">
+                    <label for="email">Email:</label>
+                    <input type="email" class="form-control" name="email">
+                  </div>
+
+                  <div class="form-group">
+                    <label for="dt_nascimento">Data de Nascimento:</label>
+                    <input type="date" class="form-control" name="dt_nascimento">
+                  </div>
+
+                  <div class="form-group">
+                    <input type="submit" class="btn btn-success">
+                  </div>
+                  
+                </form>
+                
+            </div>
+        </div>
+    </div>
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+  </body>
 </html>
